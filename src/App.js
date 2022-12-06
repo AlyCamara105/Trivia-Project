135d8f2f2b4306d259187086f94a9f478254fa01
Aly Camara <79851069+AlyCamara105@users.noreply.github.com>
2022-12-06 00:47:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./App.css";
+import data from "./sample_data.json";
+import { Question } from "./components/Question.js";
+
+function App() {
+  let currentQuestionNumber = 0;
+  return (
+    <div className="app">
+      <h1 className="title">Trivia!</h1>
+      <Question question=data[currentQuestionNumber].question.text />
+    </div>
+  );
+}
+
+export default App;
